4fc82e1da41681c47a8dae0a597a3b1bd8c5066e
Chinh Trung <fecoder.chinh@gmail.com>
2021-07-16 22:37:25 +0700

    tái cấu trúc scripts

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const pug = require('gulp-pug');
 const sass = require('gulp-sass')(require('sass'));
 
 function html() {
-    return src(['./templates/**/*.pug', '!./templates/partials/*.pug', '!./templates/wrapper.pug'])
+    return src(['./templates/**/*.pug', '!./templates/partials/**/*.pug', '!./templates/wrapper.pug'])
         .pipe(
             pug({
                 pretty: true
@@ -13,7 +13,7 @@ function html() {
 }
 
 function css() {
-    return src('./assets/scss/**/*.scss')
+    return src(['./assets/scss/style.scss', '!./assets/scss/private/**/*.scss'])
         .pipe(sass().on('error', sass.logError))
         .pipe(dest('./css/'));
 }
